54492300cbdda081ac394057539fe32fba7a15ff
Ankit Shankar <ankitshankar@gmail.com>
2012-01-16 20:26:16 -0500

    Add signup page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,32 @@
+var signup_post = function(req, res) {
+    if (!req.body.name || !req.body.email || !req.body.house
+            || !req.body.year) {
+        return res.render('signup', {
+            form: req.body,
+            errors: ['Make sure to fill out everything!'],
+        });
+    }
+    var SUFFIX = '@college.harvard.edu';
+    if (req.body.email.indexOf(SUFFIX)
+            != req.body.email.length - SUFFIX.length) {
+        return res.render('signup', {
+            form: req.body,
+            errors: ['You must use an @college.harvard.edu email'],
+        });
+    }
+    res.send(404); // TODO
+};
+
 exports.registerOn = function(app) {
     app.get('/', function(req, res) {
-        res.render('index', { title: 'Express' })
+        res.render('index');
     });
+
+    app.get('/signup', function(req, res) {
+        res.render('signup', {
+            form: {},
+        });
+    });
+
+    app.post('/signup', signup_post);
 };
