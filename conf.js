1f1e827e5c74a11deae6660d7ca73d839a22dadb
Ankit Shankar <ankitshankar@gmail.com>
2012-01-16 23:59:58 -0500

    Add email with Sendgrid

--- conf.js
+++ conf.js
@@ -1,3 +1,12 @@
 module.exports = {
     port: process.env.PORT || 3000,
+    mongo_uri: process.env.MONGOLAB_URI || 'mongodb://localhost/veritas-vocations',
+    sendgrid: {
+        username: process.env.SENDGRID_USERNAME,
+        password: process.env.SENDGRID_PASSWORD,
+        host: 'smtp.sendgrid.net',
+        port: '587',
+    },
+    domain: 'veritasvocations.com',
+    email: 'noreply@veritasvocations.com',
 };
